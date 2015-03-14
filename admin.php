90302ee49681d1ae0ee51dd1b73a3fd7ab80637c
Conor Gilmer <conor.gilmer@gmail.com>
2015-03-14 18:13:40 +0000

    admin

--- admin.php
+++ admin.php
@@ -40,68 +40,71 @@ include (TEMPLATE_PATH . "/header.html");
 <div class="span9">
 
 <?php 
-// cross join
-$sqlQuery = "SELECT a.property_addr1,
-                    a.property_addr2,
-                    a.property_addr3,
-                    a.property_county,
-                    a.property_price,
-                    a.property_size,
-                    a.property_type,
-                    a.property_status,
-                    a.property_desc,
-                    a.property_id,
-                    a.property_ts,
-                    a.property_date_created,
-                    b.file_content,
-                    b.file_type,
-                    b.file_size,
-                    b.file_name,
-                    b.file_extension,
-                    b.title
-            FROM property a, photos b
-            where a.property_photo = b.photo_id";
+
+$sqlQuery ="select * from skiholiday";
 $result = mysql_query($sqlQuery);
+ 
 
 
 if ($result) {
     	$htmlString = "";
 
-	$counter =0; //modulo division allows 3 houses per row
-        $htmlString = "
-            <div class=\"row\">";
+	
+        $htmlString .= "
+            <div class=\"media\">";
        
 	while ($product = mysql_fetch_assoc($result))
 	{ 
-            $counter++;
-             $htmlString .="<div class=\"col-sm-6 col-md-4\">";
-    $htmlString .= "<div class=\"thumbnail\">";
-    $htmlString .=   "<img width=\"350\" height=\"350\" src=\"data:image/jpeg;base64,". 
-           base64_encode( $product['file_content'] ) . "\" />";
-      $htmlString .="<div class=\"caption\">";
-      //  $htmlString.="<h3>".$product["file_name"]."</h3>";
-        $htmlString.="<p><strong>Address</strong>".$product["property_addr1"]."<br>";
-        $htmlString.= $product["property_addr2"]."<br>";
-        $htmlString.= $product["property_addr3"]."</p>";
-        $htmlString.="<p><strong>County </strong> ".getCounty($product["property_county"])."</p>";
-        $htmlString.="<p><strong>Description </strong>".$product["property_desc"]."</p>";
-        $htmlString.="<p><strong>Status </strong>".$product["property_status"]."</p>";
-        $htmlString.="<p><strong>Price &euro; </strong>".$product["property_price"]."</p>";
-         $dateposted = date('d-m-Y',strtotime($product['property_ts']));
-        $htmlString.="<p><strong>Date Edited: </strong> ". $dateposted . "</p>";
-         $datecreated = date('d-m-Y',strtotime($product['property_date_created']));
-        $htmlString.="<p><strong>Date Posted: </strong> ". $datecreated . "</p>";
-        $htmlString.="<p><a href=\"propertydetails.php?property_id=".$product['property_id']."\" class=\"btn btn-primary\" role=\"button\">Property Details</a> </p>";
-    //        <a href=\"#\" class=\"btn btn-default\" role=\"button\">Button</a></p>";
+              $statusString="<p><strong>Place </strong>".$product["ski_place"]."</p>";
+      
+      $ski_image = $product["ski_photo"];   
+      if (empty($ski_image)) {
+          $ski_image= "nophoto.png";
+      }
+      $htmlString .= "<div class=\"thumbnail\"/>";      
+      $htmlString .= "<img class=\"media=object\" align=right width=250 height=250 src = \"images/".$ski_image."\" />";
+/*    $htmlString .=   "<div class=\"thumbnail\"> <a class=\"pull-right\" href=\"#\">
+    <img class=\"media-object\" width=150 height=150 src=\"data:image/jpeg;base64,". 
+           base64_encode( $product['file_content'] ) . "\" /></a>";
+  */
+        $htmlString .="<div class=\"media-body\">";
+      
+      
+        $htmlString.="<h4 class=\"media-heading\"><strong>Place:  </strong>".$product["ski_place"].", ";
+        $htmlString.= $product["ski_country"]."</h4>";
+        $holiday_date = date('d-m-Y',strtotime($product["ski_date"]));
+
+       //         ", <strong>County </strong> ".getCounty($product["property_county"])."</h4>";
+        $htmlString.="<table><tr><td Style=\"white-space: normal; width:400px;\">";
+        $htmlString.="<p><strong>Holiday No. </strong>".$product["ski_id"]."</p>";
+        $htmlString.="<p><strong>Date: </strong>" . $holiday_date."</p>";
+       
+        $htmlString.="<p><strong>Type </strong>".$product["holiday_type"]."</p>";
+        $htmlString.="<p><strong>Description </strong>".$product["ski_desc"]."</p>";
+      
+        $htmlString.="<p><strong>Days </strong>".$product["ski_days"]."</p>";
+//       $htmlString.=$statusString; 
+  //     $htmlString.="<p><strong>Price &euro; </strong>".$product["property_price"]."</p>";
+        $htmlString.="</td><td>";
+        $htmlString.="<p><strong>Ski Costs</strong></p>";
+        $htmlString.="<p><strong>Flights </strong>".$product["ski_days"]."</p>";
+        $htmlString.="<p><strong>Transfers </strong>".$product["ski_days"]."</p>";
+        $htmlString.="<p><strong>Accommodation </strong>".$product["ski_days"]."</p>";
+                $htmlString.="<p><strong>Ski Rental </strong>".$product["ski_days"]."</p>";
+        $htmlString.="<p><strong>Ski Pass </strong>".$product["ski_days"]."</p>";
+        $htmlString.="<p><strong>Total </strong>".$product["ski_days"]."</p>";
+
+        //     $dateposted = date('d-m-Y',strtotime($product['property_ts']));
+        //$htmlString.="<p><strong>Date Posted: </strong> ". $dateposted . "</p>";
+   
+        $htmlString.="</td></tr></table>";
+        $htmlString.="<p><a href=\"skidetailsopen.php?ski_id=".$product['ski_id']."\" class=\"btn btn-primary\" role=\"button\">Holiday Details</a> </p>";
+//           <a href=\"#\" class=\"btn btn-default\" role=\"button\">Button</a></p>";
       $htmlString.="</div>
     </div>
-  </div>";
+  ";
       
-		$htmlString .=  "</tr>\n";
-		if (($counter % 3) ==0) {
-                    $htmlString.= " </div> <div class=\"row\">"; // carriage return new line on grid!
-                } // if
-	
+		
                 
                 } // While
 	$htmlString .=  "</div>";
